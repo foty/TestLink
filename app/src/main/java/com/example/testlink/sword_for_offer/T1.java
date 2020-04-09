1a90a9f875c7e68857a4b2ee8a3a92616a9b58a2
foty <534728972@qq.com>
2020-04-09 18:15:53 +0800

    添加仓库

---
+++ app/src/main/java/com/example/testlink/sword_for_offer/T1.java
@@ -0,0 +1,9 @@
+package com.example.testlink.sword_for_offer;
+
+/**
+ * Create by lxx
+ * Date : 2020/4/9 17:50
+ * Use by 赋值运算
+ */
+public class T1 {
+}
---
+++ app/src/main/java/com/example/testlink/sword_for_offer/T2.java
@@ -0,0 +1,20 @@
+package com.example.testlink.sword_for_offer;
+
+/**
+ * Create by lxx
+ * Date : 2020/4/9 17:51
+ * Use by 单例设计函数
+ */
+public class T2 {
+
+//    public class Singleton {
+//        private static class SingletonHodler {
+//            private static Singleton ourInstance = new Singleton();
+//        }
+//        public static Singleton getInstance() {
+//            return SingletonHodler.ourInstance;
+//        }
+//        private Singleton() {
+//        }
+//    }
+}
