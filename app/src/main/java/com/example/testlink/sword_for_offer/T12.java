17e5381ff8134b65de962d69a604799ecf456962
foty <534728972@qq.com>
2020-05-09 18:16:31 +0800

    T12

---
+++ app/src/main/java/com/example/testlink/sword_for_offer/T12.java
@@ -0,0 +1,15 @@
+package com.example.testlink.sword_for_offer;
+
+/**
+ * Create by lxx
+ * Date : 2020/5/9 15:17
+ * Use by 打印从1到最大的n位数。
+ */
+public class T12 {
+
+    /**
+     * 问题:输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。
+     * 比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+     *
+     */
+}
