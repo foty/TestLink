41c7dfb1615d86c933ea701eac1ce7b7cd328312
foty <534728972@qq.com>
2020-05-19 15:58:27 +0800

    T16

---
+++ app/src/main/java/com/example/testlink/sword_for_offer/T16.java
@@ -0,0 +1,27 @@
+package com.example.testlink.sword_for_offer;
+
+/**
+ * Create by lxx
+ * Date : 2020/5/19 15:43
+ * Use by 输出反转后的链表
+ */
+public class T16 {
+
+    /**
+     * 双指针法(循环遍历，交换指向-方向输出即可)
+     *
+     * @param head
+     * @return
+     */
+    private ListNode solve(ListNode head){
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
