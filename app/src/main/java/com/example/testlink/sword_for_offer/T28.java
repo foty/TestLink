78aef9d240fcaecc4cbffb5c6fa492d3cc11b50b
foty <534728972@qq.com>
2020-07-08 15:00:50 +0800

    T28(end)

--- app/src/main/java/com/example/testlink/sword_for_offer/T28.java
+++ app/src/main/java/com/example/testlink/sword_for_offer/T28.java
@@ -1,7 +1,7 @@
 package com.example.testlink.sword_for_offer;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Create by lxx
@@ -14,105 +14,65 @@ public class T28 {
         String[] abcs = solve("abcde");
         System.out.println(17 * 7 + 1);
         System.out.println(abcs.length);
-//        System.out.println(count);
+
         for (int i = 0; i < abcs.length; i++) {
             System.out.println(abcs[i]);
         }
     }
 
 
+//    执行用时：26 ms, 在所有 Java 提交中击败了41.40%的用户
+//    内存消耗：44.3 MB, 在所有 Java 提交中击败了100.00%的用户
+    /**
+     * 思路： 递归。
+     * 容易忽略的点：字符重复 --> (去重)
+     *
+     * @param s
+     * @return
+     */
     public static String[] solve(String s) {
-//        if (null == s || s.length() <= 0) {
-//            return new String[0];
-//        }
-//
-//        if (s.length() == 1) {
-//            String[] sirs = new String[1];
-//            sirs[0] = s;
-//            return sirs;
-//        }
-
-
-        List<String> list = new ArrayList<>();
-        char[] chars = s.toCharArray();
-
-        for (int i = 0; i < chars.length; i++) {
-            if (i == 0) {
-
-            } else {
-                char temp = chars[0];
-                chars[0] = chars[i];
-                chars[i] = temp;
-            }
-            reSwap(1, s, list);
+        if (null == s || s.length() <= 0) {
+            return new String[0];
         }
 
-
-        String[] sss = new String[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            sss[i] = list.get(i);
+        if (s.length() == 1) {
+            String[] sirs = new String[1];
+            sirs[0] = s;
+            return sirs;
         }
-        return sss;
+
+        Set<String> list = new HashSet<>();
+        reSwap(0, s, list);
+
+        return list.toArray(new String[0]);
     }
 
-    static int count;
 
-
-    private static void reSwap(int index, String s, List<String> list) {
+    private static void reSwap(int index, String s, Set<String> list) {
         char[] chars = s.toCharArray();
-        if (index < s.length() - 2) {
+        if (index < s.length() - 2) {  //终止递归条件(进入到了字符串最后2位时，交换位置)
             reSwap(index + 1, s, list);
-            swap2(list, chars, index, index + 1);
+
+            //交换一个位置后，循环交换后面每个位置的字符。
+            for (int i = index + 1; i < chars.length; i++) {
+                swap(list, chars, index, i);
+            }
 
         } else {
             list.add(s);
-            swap2(list, chars, index, index + 1);
+            swap(list, chars, index, index + 1);
         }
     }
 
-    private static void swap(List<String> list, char[] chars, int start) {
-
-        char temp = chars[start];
-        chars[start] = chars[start + 1];
-        chars[start + 1] = temp;
-        count++;
-        list.add(String.valueOf(chars));
-    }
-
-    private static void swap2(List<String> list, char[] chars, int start, int end) {
+    private static void swap(Set<String> list, char[] chars, int start, int end) {
         char temp = chars[start];
         chars[start] = chars[end];
         chars[end] = temp;
 
-        if (start < chars.length - 2) {
+        if (start < chars.length - 2) {// 交换位置后，后续是否需要递归
             reSwap(start + 1, String.valueOf(chars), list);
         } else {
             list.add(String.valueOf(chars));
         }
     }
-
-    /**
-     * ["abcde","abced",
-     * "abdce","abdec","abecd","abedc",
-     * "acbde","acbed","acdbe","acdeb","acebd","acedb",
-     * "adbce","adbec","adcbe","adceb","adebc","adecb",
-     * "aebcd","aebdc","aecbd","aecdb","aedbc","aedcb",
-     *
-     *
-     *                         "bacde","baced","badce","badec",
-     * "baecd","baedc","bcade","bcaed","bcdae","bcdea","bcead",
-     * "bceda","bdace","bdaec","bdcae","bdcea","bdeac","bdeca",
-     * "beacd","beadc","becad","becda","bedac","bedca","cabde",
-     * "cabed","cadbe","cadeb","caebd","caedb","cbade","cbaed",
-     * "cbdae","cbdea","cbead","cbeda","cdabe","cdaeb","cdbae",
-     * "cdbea","cdeab","cdeba","ceabd","ceadb","cebad","cebda",
-     * "cedab","cedba","dabce","dabec","dacbe","daceb","daebc",
-     * "daecb","dbace","dbaec","dbcae","dbcea","dbeac","dbeca",
-     * "dcabe","dcaeb","dcbae","dcbea","dceab","dceba","deabc",
-     * "deacb","debac","debca","decab","decba","eabcd","eabdc",
-     * "eacbd","eacdb","eadbc","eadcb","ebacd","ebadc","ebcad",
-     * "ebcda","ebdac","ebdca","ecabd","ecadb","ecbad","ecbda","
-     * ecdab","ecdba","edabc","edacb","edbac","edbca","edcab",
-     * "edcba"]
-     */
 }
