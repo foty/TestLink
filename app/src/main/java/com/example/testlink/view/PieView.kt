823f2b075082f0915b69f00579daf8f4b106b9ca
foty <534728972@qq.com>
2020-09-10 16:02:14 +0800

    添加一个饼状图view完善

--- app/src/main/java/com/example/testlink/view/PieView.kt
+++ app/src/main/java/com/example/testlink/view/PieView.kt
@@ -1,9 +1,7 @@
 package com.example.testlink.view
 
-import android.R
 import android.content.Context
 import android.graphics.*
-import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.util.DisplayMetrics
 import android.view.View
@@ -11,6 +9,7 @@ import android.view.WindowManager
 import org.jetbrains.annotations.Nullable
 import java.text.DecimalFormat
 import java.util.*
+import kotlin.math.abs
 import kotlin.math.roundToInt
 
 
@@ -21,94 +20,128 @@ import kotlin.math.roundToInt
  */
 class PieView : View {
 
-    // https://yq.aliyun.com/articles/638335
+    private val mContext: Context = context
 
-    private val rectF: RectF by lazy {
-        RectF(centerX - radius, 0f, centerX + radius, radius * 2)
-    }
-    private val mContext: Context = context!!
+    private var mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    private var mPaint: Paint? = null
-
-    //每块占比的绘制的颜色
+    /**
+     * 每块占比的绘制的颜色
+     */
     private var mColorList: MutableList<Int> = ArrayList()
 
-    //圆弧占比的集合
+    /**
+     * 圆弧占比的集合
+     */
     private var mRateList: MutableList<Float> = ArrayList()
 
-    //是否展示文字
-    private var isShowRateText = false
+    /**
+     * 是否展示文字
+     */
+    private var isShowRateText = true
 
-    //圆弧半径
+    /**
+     * 圆弧半径
+     */
     private var radius = 0f
-    private var startAngle = 0
+    private var startAngle = 0f
 
-    //不同色块之间是否需要空隙offset
+    /**
+     * 圆心x坐标
+     */
+    private var centerX = 0f
+
+    /**
+     * 圆心y坐标
+     */
+    private var centerY = 0f
+
+    private val rectF: RectF by lazy {
+        RectF(centerX - radius, centerY - radius, centerX + radius, centerY + radius)
+    }
+
+    /**
+     * 色块间留白
+     */
     private val offset = 0
 
-    //圆弧中心点小圆点的圆心半径
-    private var centerPointRadius = 0
-    private var showRateSize = 0
+    /**
+     * 圆弧中心点圆点的圆心半径
+     */
+    private val centerPointRadius by lazy {
+        dip2px(mContext, 1)
+    }
+
+    private val textSize by lazy {
+        dip2px(mContext, 7)
+    }
+
     private var textRect: Rect? = null
 
-    //折线横向长度
-    private var xOffset = 0f
+    /**
+     * 折线横向长度
+     */
+    private val xOffset by lazy {
+        dip2px(mContext, 20)
+    }
 
-    //折线偏Y方向长度
-    private var yOffset = 0f
-    private val mChangeAngle = 0f
-    private val isAnimation = false
-    private val sign = 0f
+    /**
+     * 折线偏Y方向长度
+     */
+    private val yOffset by lazy {
+        dip2px(mContext, 10)
+    }
 
-    private var centerX = 0f
-    private var centery = 0f
+    private var mPointList = ArrayList<Point>()
+    private var lastPoint: Point? = null
 
+    private var rby = 10
+    private var lby = 10
+    private var lty = 10
+    private var rty = 10
 
     constructor(context: Context?) : this(context, null)
 
     constructor(context: Context?, @Nullable attrs: AttributeSet?) : this(context, attrs, 0)
 
-    constructor(context: Context?, @Nullable attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-
-    init {
+    constructor(context: Context?, @Nullable attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         init()
         initData()
     }
 
     private fun initData() {
-        val rate = floatArrayOf(30f, 40f, 15f, 15f)
-        val colors = intArrayOf(Color.RED, Color.BLUE, Color.YELLOW, Color.GRAY)
+        val rate = floatArrayOf(12f, 1f, 1f, 1f, 10f, 7f, 1f, 1f, 1f, 15f, 13f, 1f, 1f, 1f, 9f, 17f, 1f, 1f, 1f, 5f)
+        val colors = intArrayOf(Color.RED, Color.BLUE, Color.YELLOW, Color.GRAY, Color.GREEN,
+
+                Color.BLACK, Color.parseColor("#ee34ff"), Color.parseColor("#653408"),
+                Color.parseColor("#666666"), Color.parseColor("#e57822"),
+
+                Color.parseColor("#2e45ee"), Color.parseColor("#e6c456"), Color.parseColor("#cc8802"),
+                Color.parseColor("#999999"), Color.parseColor("#c6c6c6"),
+
+                Color.parseColor("#FFA500"), Color.parseColor("#FFB46BC7"), Color.parseColor("#FF3F857F"),
+                Color.parseColor("#FF14A01A"), Color.parseColor("#FFF06539"))
+
         for (i in rate.indices) {
             mRateList.add(rate[i] / 100)
             mColorList.add(colors[i])
         }
         textRect = Rect()
         if (mRateList.size > 0) {
-            mPaint?.getTextBounds(mRateList[0].toString() + "%", 0, (mRateList[0].toString() + "%").length, textRect)
+            mPaint.getTextBounds(mRateList[0].toString() + "%", 0, (mRateList[0].toString() + "%").length, textRect)
         }
     }
 
     private fun init() {
 
-        centerX = getScreenWidth() / 2.toFloat()
         radius = dip2px(mContext, 100)
+        centerX = getScreenWidth() / 2.toFloat()
+        centerY = radius + 80f
 
-        centerPointRadius = dip2px(mContext, 2).toInt()
-
-        xOffset = dip2px(mContext, 20)
-        yOffset = dip2px(mContext, 5)
-
-        showRateSize = dip2px(mContext, 10).toInt()
-
-        mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        mPaint!!.color = Color.RED
-        mPaint!!.isAntiAlias = true
-        mPaint!!.style = Paint.Style.FILL
-        mPaint!!.textSize = showRateSize.toFloat()
-
-        if (mRateList.size > 0) {
-            textRect = Rect()
-            mPaint!!.getTextBounds(mRateList[0].toString() + "%", 0, (mRateList[0].toString() + "%").length, textRect)
+        mPaint.let {
+            it.color = Color.RED
+            it.isAntiAlias = true
+            it.style = Paint.Style.FILL
+            it.textSize = textSize
         }
     }
 
@@ -135,45 +168,28 @@ class PieView : View {
 
 
     override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
 
-        //(1)绘制圆饼
-//        val rectF = RectF(0 + centerPointRadius + (xOffset + yOffset + textRect!!.width()),
-//                0 + centerPointRadius + (xOffset + yOffset + textRect!!.height()),
-//                2 * radius + centerPointRadius + (xOffset + yOffset + textRect!!.width()),
-//                2 * radius + centerPointRadius + (xOffset + yOffset + textRect!!.height()))
-
-        val mPointList: MutableList<Point> = ArrayList()
+        mPointList.clear()
 
         for (i in mRateList.indices) {
-            mPaint?.style = Paint.Style.FILL
-            mPaint?.color = mColorList[i]
-            // 画弧长
-            canvas.drawArc(rectF, startAngle.toFloat(), (mRateList[i] * 360) - offset, true, mPaint)
+            mPaint.style = Paint.Style.FILL
+            mPaint.color = mColorList[i]
 
-            //(2)处理每块圆饼弧的中心点，绘制折线，显示对应的文字
+            canvas.drawArc(rectF, startAngle, (mRateList[i] * 360) - offset, true, mPaint)
+
             if (isShowRateText) {
-                // 绘制折线
-                dealPoint(rectF, startAngle.toFloat(), (mRateList[i] * 360 - offset) / 2, mPointList)
+                dealPoint(rectF, startAngle, (mRateList[i] * 360 - offset) / 2, mPointList)
 
-                val point: Point = mPointList[i]
-                mPaint?.color = Color.BLACK //点的绘制的颜色
-                canvas.drawCircle(point.x.toFloat(), point.y.toFloat(), centerPointRadius.toFloat(), mPaint)
+                mPaint.color = mColorList[i]
 
-                // 绘制文字
-                dealRateText(canvas, point, i, mPointList)
+                canvas.drawCircle(mPointList[i].x.toFloat(), mPointList[i].y.toFloat(), centerPointRadius, mPaint)
+                dealRateText(canvas, mPointList[i], i, mPointList)
             }
-            startAngle += (mRateList[i] * 360).toInt()
-        }
 
-        //(3)绘制内部中空的圆
-//        mPaint?.setColor(ContextCompat.getColor(this!!.mContext!!, R.color.holo_purple))
-//        mPaint?.setStyle(Paint.Style.FILL)
-//        canvas.drawCircle(radius + centerPointRadius + (xOffset + yOffset + textRect!!.width()),
-//                radius + centerPointRadius + (xOffset + yOffset + textRect!!.height()), radius / 1.5f, mPaint)
+            startAngle += mRateList[i] * 360
+        }
     }
 
-    private var lastPoint: Point? = null
 
     private fun dealRateText(canvas: Canvas, point: Point, position: Int, pointList: List<Point>) {
         lastPoint = if (position == 0) {
@@ -181,104 +197,124 @@ class PieView : View {
         } else {
             pointList[position - 1]
         }
+
         val floats = FloatArray(8)
         floats[0] = point.x.toFloat()
         floats[1] = point.y.toFloat()
+
         //右半圆
-        if (point.x >= radius + centerPointRadius + (xOffset + yOffset + textRect!!.width())) {
-            mPaint?.setTextAlign(Paint.Align.LEFT)
-            floats[6] = point.x + xOffset
-            //防止相邻的圆饼绘制的文字重叠显示
-//            if (lastPoint != null) {
-//                int absX = Math.abs(point.x - lastPoint.x);
-//                int absY = Math.abs(point.y - lastPoint.y);
-//                if (absX > 0 && absX < 20 && absY > 0 && absY < 20) {
-//                    floats[6] = point.x + xOffset - textRect.width() / 2;
-//                    Log.e("TAG", "右半圆");
-//                } else {
-//                    floats[6] = point.x + xOffset;
-//                }
-//            } else {
-//                floats[6] = point.x + xOffset;
-//            }
-            if (point.y <= radius + centerPointRadius + (xOffset + yOffset + textRect!!.height())) {
+        if (point.x >= centerX) {
+            mPaint.textAlign = Paint.Align.LEFT
+
+            if (point.y <= centerY) {
                 //右上角
                 floats[2] = point.x + yOffset
                 floats[3] = point.y - yOffset
                 floats[4] = point.x + yOffset
                 floats[5] = point.y - yOffset
+
+                floats[6] = point.x + xOffset
                 floats[7] = point.y - yOffset
+
+                if (offSetAngle(point, lastPoint)) {
+                    floats[6] = point.x + xOffset - rty
+                    floats[7] = point.y - yOffset - rty
+                    rty += 5
+                } else {
+                    rty = 10
+                }
+
+
             } else {
                 //右下角
                 floats[2] = point.x + yOffset
                 floats[3] = point.y + yOffset
                 floats[4] = point.x + yOffset
                 floats[5] = point.y + yOffset
+
+                floats[6] = point.x + xOffset
                 floats[7] = point.y + yOffset
+
+                if (offSetAngle(point, lastPoint)) {
+                    floats[6] = point.x + xOffset + rby
+                    floats[7] = point.y + yOffset + rby
+                    rby += 5
+                } else {
+                    rby = 10
+                }
             }
-            //左半圆
-        } else {
-            mPaint?.setTextAlign(Paint.Align.RIGHT)
-            floats[6] = point.x - xOffset
-            //防止相邻的圆饼绘制的文字重叠显示
-//            if (lastPoint != null) {
-//                int absX = Math.abs(point.x - lastPoint.x);
-//                int absY = Math.abs(point.y - lastPoint.y);
-//                if (absX > 0 && absX < 20 && absY > 0 && absY < 20) {
-//                    floats[6] = point.x - xOffset - textRect.width() / 2;
-//                    Log.e("TAG", "左半圆");
-//                } else {
-//                    floats[6] = point.x - xOffset;
-//                }
-//            } else {
-//                floats[6] = point.x - xOffset;
-//            }
-            if (point.y <= radius + centerPointRadius) {
+
+        } else { //左半圆
+            mPaint.textAlign = Paint.Align.RIGHT
+
+            if (point.y <= centerY) {
                 //左上角
                 floats[2] = point.x - yOffset
                 floats[3] = point.y - yOffset
                 floats[4] = point.x - yOffset
                 floats[5] = point.y - yOffset
+
+                floats[6] = point.x - xOffset
                 floats[7] = point.y - yOffset
+
+                if (offSetAngle(point, lastPoint)) {
+                    floats[6] = point.x - xOffset - lty
+                    floats[7] = point.y - yOffset - lty
+                    lty += 5
+                } else {
+                    lty = 10
+                }
+
             } else {
                 //左下角
                 floats[2] = point.x - yOffset
                 floats[3] = point.y + yOffset
                 floats[4] = point.x - yOffset
                 floats[5] = point.y + yOffset
+
+                floats[6] = point.x - xOffset
                 floats[7] = point.y + yOffset
+
+                if (offSetAngle(point, lastPoint)) {
+                    floats[6] = point.x - xOffset - lby
+                    floats[7] = point.y + yOffset - lby
+                    lby += 6
+                } else {
+                    lby = 10
+                }
             }
         }
-        //根据每块的颜色，绘制对应颜色的折线
-//        mPaint.setColor(mRes.getColor(colorList.get(position)));
-        mPaint?.setColor(ContextCompat.getColor(this!!.mContext!!, R.color.darker_gray))
-        //画圆饼图每块边上的折线
+
+        mPaint.color = Color.BLACK
         canvas.drawLines(floats, mPaint)
-        mPaint?.setStyle(Paint.Style.STROKE)
-        //绘制显示的文字,需要根据类型显示不同的文字
+        mPaint.style = Paint.Style.STROKE
+
         if (mRateList.size > 0) {
             //Y轴：+ textRect.height() / 2 ,相对沿线居中显示
-            canvas.drawText(getFormatPercentRate(mRateList[position] * 100) + "%", floats[6], floats[7] + textRect!!.height() / 2, mPaint)
+            canvas.drawText(getFormatPercentRate(mRateList[position] * 100) + "%", floats[6],
+                    floats[7] + textRect!!.height() / 2, mPaint)
         }
     }
 
+    /**
+     * PathMeasure: 测量一段path的长度
+     * getPosTan: 返回指定距离的正切值。
+     */
     private fun dealPoint(rectF: RectF, startAngle: Float, endAngle: Float, pointList: MutableList<Point>) {
         val path = Path()
-        //通过Path类画一个90度（180—270）的内切圆弧路径
         path.addArc(rectF, startAngle, endAngle)
         val measure = PathMeasure(path, false)
-        // Log.e("路径的测量长度:", "" + measure.getLength());
+
         val coords = floatArrayOf(0f, 0f)
-        //利用PathMeasure分别测量出各个点的坐标值coords
-        val divisor = 1
-        measure.getPosTan(measure.length / divisor, coords, null)
-        //        Log.e("coords:", "x轴:" + coords[0] + " -- y轴:" + coords[1]);
-        val x = coords[0]
-        val y = coords[1]
-        val point = Point(x.roundToInt(), y.roundToInt())
+        measure.getPosTan(measure.length / 1, coords, null)
+
+        val point = Point(coords[0].roundToInt(), coords[1].roundToInt())
         pointList.add(point)
     }
 
+    /**
+     *
+     */
     private fun updateDate(rateList: MutableList<Float>, colorList: MutableList<Int>, isShowRateText: Boolean) {
         this.isShowRateText = isShowRateText
         mRateList = rateList
@@ -291,7 +327,7 @@ class PieView : View {
      * 获取格式化的保留两位数的数
      */
     private fun getFormatPercentRate(dataValue: Float): String {
-        val decimalFormat = DecimalFormat(".00") //构造方法的字符格式这里如果小数不足2位,会以0补足.
+        val decimalFormat = DecimalFormat("0.00") //构造方法的字符格式这里如果小数不足2位,会以0补足.
         return decimalFormat.format(dataValue)
     }
 
@@ -300,10 +336,40 @@ class PieView : View {
     private fun px2dp(context: Context, px: Float): Float = px / context.resources.displayMetrics.density
 
     /**
+     *
+     */
+    private fun offSetAngle(currentP: Point, lastP: Point?): Boolean {
+        if (lastP != null) {
+            if (!halfArea(currentP, lastP)) return false
+
+            val absX = abs(currentP.x - lastPoint!!.x)
+            val absY = abs(currentP.y - lastPoint!!.y)
+            if (absY in 1..15) {
+                return true
+            } else {
+                rby = 10
+                lby = 10
+            }
+        }
+        return false
+    }
+
+    private fun halfArea(currentP: Point, lastP: Point?): Boolean {
+
+        if (lastP != null) {
+            return (currentP.x > centerX && lastP.x > centerX || currentP.x < centerX && lastP.x < centerX)
+                    && (
+                    currentP.y > centerY && lastP.y > centerY || currentP.y < centerY && lastP.y < centerY)
+        }
+        return true
+
+    }
+
+    /**
      * 获取屏幕宽度
      *
      */
-    fun getScreenWidth(): Int {
+    private fun getScreenWidth(): Int {
         val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         val outMetrics = DisplayMetrics()
         wm.defaultDisplay.getMetrics(outMetrics)
