49358b49b64d7bc01047a2f8df178a8414a4f093
foty <534728972@qq.com>
2020-11-03 17:39:43 +0800

    添加目录

---
+++ app/src/main/java/com/example/testlink/askdays/AskDemo.java
@@ -0,0 +1,9 @@
+package com.example.testlink.askdays;
+
+/**
+ * Create by lxx
+ * Date : 2020/11/3 9:08
+ * Use by 源自玩安卓每日一问
+ */
+public class AskDemo {
+}
---
+++ app/src/main/java/com/example/testlink/askdays/ask.md
@@ -0,0 +1,6 @@
+1. 内存泄漏的注意点  
+都是发送端  
+2. 自定义View流程  
+
+3. 并发专题: volatile、synchronize、cas、happens、before、lost wake up等。
+
