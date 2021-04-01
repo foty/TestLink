9ccd5360c85ffa0620980945bf68d8d78167cca0
foty <534728972@qq.com>
2021-04-01 10:18:52 +0800

    4-1、笨阶乘

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -913,7 +913,7 @@ public class Topics {
     }
 
     /**
-     * 190、颠倒二进制位
+     * 190、颠倒二进制位 ?
      */
     public int test190() {
         /**
@@ -1012,7 +1012,7 @@ public class Topics {
     }
 
     /**
-     * 90、子集II(3.31)
+     * 90、子集II(3.31) ?
      */
     public List<List<Integer>> subsetsWithDup(int[] nums) {
 
@@ -1023,6 +1023,58 @@ public class Topics {
 
 
         return lists;
+    }
+
+    /**
+     * 1006、笨阶乘
+     */
+    public int clumsy(int N) {
+        /**
+         * 通常，正整数 n 的阶乘是所有小于或等于 n 的正整数的乘积。例如，factorial(10) = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1。
+         * 相反，我们设计了一个笨阶乘 clumsy：在整数的递减序列中，我们以一个固定顺序的操作符序列来依次替换原有的乘法操作符：乘法(*)，除法(/)，
+         * 加法(+)和减法(-)。例如，clumsy(10) = 10 * 9 / 8 + 7 - 6 * 5 / 4 + 3 - 2 * 1。然而，这些运算仍然使用通常的算术运算顺序：
+         * 我们在任何加、减步骤之前执行所有的乘法和除法步骤，并且按从左到右处理乘法和除法步骤。另外，我们使用的除法是地板除法（floor division），
+         * 所以 10 * 9 / 8 等于 11。这保证结果是一个整数。实现上面定义的笨函数：给定一个整数 N，它返回 N 的笨阶乘。
+         *
+         *  提示：
+         * 1 <= N <= 10000
+         * -2^31 <= answer <= 2^31 - 1  （答案保证符合 32 位整数。）
+         */
+
+        // 4*3/2+1
+
+        String[] op = new String[]{"*", "/", "+", "-"};
+        int index = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(N);
+        for (int i = N - 1; i > 0; i--) {
+            switch (op[index]) {
+                case "*":
+                    stack.push(i * stack.pop());
+                    break;
+                case "/":
+                    stack.push(stack.pop()/i);
+                    break;
+
+                case "+":
+                    stack.push(i);
+                    break;
+                case "-":
+                    stack.push(i * -1); // 转换成负数，最后统一相加
+                    break;
+            }
+            index = (index + 1) % 4;
+        }
+
+        int sum = 0;
+        while (!stack.isEmpty()){
+            sum += stack.pop();
+        }
+
+        return sum;
+
+//        执行用时：16 ms, 在所有 Java 提交中击败了 7.74% 的用户
+//        内存消耗：37.5 MB, 在所有 Java 提交中击败了 18.06% 的用户
     }
 }
 
