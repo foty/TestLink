f922b4d789e41a6b8df69679cfb1eae67a88011d
foty <534728972@qq.com>
2021-05-06 10:37:50 +0800

    5-6 每日 (51假日没有搞，废了废了)

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -1992,5 +1992,44 @@ public class Topics {
     }
 
 
+    /**
+     * 1720、解码异或后的数组
+     */
+    public int[] decode(int[] encoded, int first) {
+        /**
+         * 未知 整数数组 arr 由 n 个非负整数组成。
+         * 经编码后变为长度为 n - 1 的另一个整数数组 encoded ，其中 encoded[i] = arr[i] XOR arr[i + 1] 。例如，arr = [1,0,2,1] 经编码后得
+         * 到 encoded = [1,2,3] 。给你编码后的数组 encoded 和原数组 arr 的第一个元素 first（arr[0]）。请解码返回原数组 arr 。可以证明答案
+         * 存在并且是唯一的。
+         *
+         * 示例 1：
+         *
+         * 输入：encoded = [1,2,3], first = 1
+         * 输出：[1,0,2,1]
+         * 解释：若 arr = [1,0,2,1] ，那么 first = 1 且 encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+         *
+         * 提示：
+         * 2 <= n <= 104
+         * encoded.length == n - 1
+         * 0 <= encoded[i] <= 105
+         * 0 <= first <= 105
+         */
+
+        /**
+         * 思路:
+         * 利用^的性质即可。一个数2次^上同一个数等于原数，即a^b^b = a.
+         *  a^b = c, c^a = a^b^a = b。
+         */
+
+        int[] result = new int[encoded.length + 1];
+        result[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            result[i + 1] = encoded[i] ^ result[i];
+        }
+        return result;
+//        执行用时：2 ms, 在所有 Java 提交中击败了 73.79% 的用户
+//        内存消耗：39.2 MB, 在所有 Java 提交中击败了 91.13% 的用户
+    }
+
 }
 
