8ead50a82f69f093992383c79c55307d8f0cdf13
foty <534728972@qq.com>
2021-05-15 11:43:25 +0800

    3、14 每日打卡

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -2379,5 +2379,110 @@ public class Topics {
         }
         return dp[steps][0];
     }
+
+    /**
+     * 12、整数转罗马数字
+     */
+    public String intToRoman(int num) {
+        /**
+         * 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+         * 字符          数值
+         * I             1
+         * V             5
+         * X             10
+         * L             50
+         * C             100
+         * D             500
+         * M             1000
+         * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+         * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5
+         * 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+         *
+         * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+         * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+         * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+         * 给你一个整数，将其转为罗马数字。
+         *  
+         * 示例 1:
+         * 输入: num = 3
+         * 输出: "III"
+         *
+         * 示例 2:
+         * 输入: num = 58
+         * 输出: "LVIII"
+         * 解释: L = 50, V = 5, III = 3.
+         *
+         * 示例 3:
+         * 输入: num = 1994
+         * 输出: "MCMXCIV"
+         * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+         *  
+         * 提示：
+         * 1 <= num <= 3999
+         */
+
+        /**
+         * 思路
+         * 纯数学解法1：
+         * 根据题目意思对num依次分解，从而求出对应位数上的罗马值。难度不高，在于要分情况判断
+         */
+
+        List<String> list = new ArrayList<>();
+        String[] flag5 = new String[]{"V", "L", "D"};
+        String[] flag1 = new String[]{"I", "X", "C", "M"};
+        int count1 = 0;
+        int count5 = 0;
+        while (num != 0) {
+            int s = num % 10;
+            if (s == 4) {
+                list.add(flag1[count1] + flag5[count5]);
+            } else if (s == 9) {
+                list.add(flag1[count1] + flag1[count1 + 1]); //进位
+            } else if (s != 0) {
+                StringBuffer sb = new StringBuffer();
+                int i1 = s - 5;
+                if (i1 >= 0) {
+                    sb.append(flag5[count5]);
+                    for (int i = 0; i < i1; i++) {
+                        sb.append(flag1[count1]);
+                    }
+                } else {
+                    for (int i = 0; i < s; i++) {
+                        sb.append(flag1[count1]);
+                    }
+                }
+                list.add(sb.toString());
+            }
+            num = num / 10;
+            count5++;
+            count1++;
+        }
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            sb2.append(list.get(i));
+        }
+        return sb2.toString();
+//        执行用时：8 ms, 在所有 Java 提交中击败了 22.71% 的用户
+//        内存消耗：38.4 MB, 在所有 Java 提交中击败了 29.33% 的用户
+
+        /**
+         * 解法2：
+         * 由于数据量不大，直接暴力求出每个位置上对应的数添加完成。
+         */
+//        String[] thousands = {"", "M", "MM", "MMM"};
+//        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+//        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+//        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+//
+//        StringBuffer roman = new StringBuffer();
+//        roman.append(thousands[num / 1000]);
+//        roman.append(hundreds[num % 1000 / 100]);
+//        roman.append(tens[num % 100 / 10]);
+//        roman.append(ones[num % 10]);
+//        return roman.toString();
+//        执行用时：6 ms, 在所有 Java 提交中击败了 49% 的用户
+//        内存消耗：38.4 MB, 在所有 Java 提交中击败了 29% 的用户
+
+    }
 }
 
