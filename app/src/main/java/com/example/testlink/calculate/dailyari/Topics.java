d53f79b18397d17c22ccdfc6bc2c3378d55b5521
foty <534728972@qq.com>
2021-05-28 09:45:41 +0800

    5-28 每日

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -3,6 +3,7 @@ package com.example.testlink.calculate.dailyari;
 import com.example.testlink.calculate.sword_for_offer.ListNode;
 import com.example.testlink.calculate.sword_for_offer.TreeNode;
 
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -3111,6 +3112,53 @@ public class Topics {
          */
 
         return Integer.bitCount(x ^ y);
+    }
+
+    /**
+     * 477、汉明距离总和
+     */
+
+    public int totalHammingDistance(int[] nums) {
+        /**
+         * 两个整数的 汉明距离 指的是这两个数字的二进制数对应位不同的数量。
+         * 计算一个数组中，任意两个数之间汉明距离的总和。
+         *
+         * 示例:
+         * 输入: 4, 14, 2
+         * 输出: 6
+         *
+         * 解释: 在二进制表示中，4表示为0100，14表示为1110，2表示为0010。（这样表示是为了体现后四位之间关系）
+         * 所以答案为：
+         * HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+         *
+         * 注意:
+         * 数组中元素的范围为从 0到 10^9。
+         * 数组的长度不超过 10^4。
+         */
+
+        /**
+         * 思路
+         * 金典暴力百分5。(使用逐位统计效率高些)
+         */
+
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                count += Integer.bitCount(nums[i] ^ nums[j]);
+            }
+        }
+        return count;
+
+        // 逐位统计法
+//        int ans = 0, n = nums.length;
+//        for (int i = 0; i < 30; ++i) {
+//            int c = 0;
+//            for (int val : nums) {
+//                c += (val >> i) & 1;
+//            }
+//            ans += c * (n - c);
+//        }
+//        return ans;
     }
 }
 
