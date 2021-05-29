aed6119098777b17c5f2c66000e10bf1490ef5ea
foty <534728972@qq.com>
2021-05-29 20:26:01 +0800

    5-29 每日打卡

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -3,11 +3,9 @@ package com.example.testlink.calculate.dailyari;
 import com.example.testlink.calculate.sword_for_offer.ListNode;
 import com.example.testlink.calculate.sword_for_offer.TreeNode;
 
-import java.time.Year;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -3159,6 +3157,63 @@ public class Topics {
 //            ans += c * (n - c);
 //        }
 //        return ans;
+    }
+
+    /**
+     * 1074. 元素和为目标值的子矩阵数量
+     */
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+
+        /**
+         * 给出矩阵 matrix 和目标值 target，返回元素总和等于目标值的非空子矩阵的数量。
+         * 子矩阵 x1, y1, x2, y2 是满足 x1 <= x <= x2 且 y1 <= y <= y2 的所有单元 matrix[x][y] 的集合。
+         * 如果 (x1, y1, x2, y2) 和 (x1', y1', x2', y2') 两个子矩阵中部分坐标不同（如：x1 != x1'），那么这两个子矩阵也不同。
+         *
+         * 示例 1：
+         * 输入：matrix = [[0,1,0],[1,1,1],[0,1,0]], target = 0
+         * 输出：4
+         * 解释：四个只含 0 的 1x1 子矩阵。
+         *
+         * 示例 2：
+         * 输入：matrix = [[1,-1],[-1,1]], target = 0
+         * 输出：5
+         * 解释：两个 1x2 子矩阵，加上两个 2x1 子矩阵，再加上一个 2x2 子矩阵。
+         *
+         * 示例 3：
+         * 输入：matrix = [[904]], target = 0
+         * 输出：0
+         *
+         * 提示：
+         * 1 <= matrix.length <= 100
+         * 1 <= matrix[0].length <= 100
+         * -1000 <= matrix[i] <= 1000
+         * -10^8 <= target <= 10^8
+         */
+        int a = matrix.length + 1;
+        int b = matrix[0].length + 1;
+        int[][] arr = new int[a][b];
+
+        int count = 0;
+        for (int i = 1; i < a; i++) {
+            for (int j = 1; j < b; j++) {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1] + matrix[i][j];
+            }
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr[0].length; j++) {
+
+                for (int k = 0; k < i; ++k) {
+                    for (int t = 0; t < j; ++t) {
+                        if ((arr[i][j] - arr[k][j] - arr[i][t] + arr[k][t]) == target) {
+                            count++;
+                        }
+                    }
+                }
+
+            }
+        }
+        return count;
     }
 }
 
