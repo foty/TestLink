ff9f16eb14a22e6cf8f64a3d4700ea864a993d8f
foty <534728972@qq.com>
2021-05-31 11:06:01 +0800

    5-31 每日

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -20,7 +20,9 @@ public class Topics {
 
     public static void main(String[] args) {
         int[] ints = new int[]{1, 2, 4, 7, 8};
-        numWays(3, 2);
+        //numWays(3, 2);
+        System.out.println(Math.sqrt(16) % 2 == 0);
+        System.out.println(1 & 0);
     }
 
 
@@ -3249,6 +3251,49 @@ public class Topics {
          * 则表示这个是2的幂。
          */
         return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    /**
+     * 342. 4的幂
+     */
+    public boolean isPowerOfFour(int n) {
+
+        /**
+         * 给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
+         * 整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4x
+         *
+         * 示例 1：
+         * 输入：n = 16
+         * 输出：true
+         *
+         * 示例 2：
+         * 输入：n = 5
+         * 输出：false
+         *
+         * 示例 3：
+         * 输入：n = 1
+         * 输出：true
+         *
+         * 提示：
+         * -231 <= n <= 231 - 1
+         */
+
+        /**
+         * 如果这个数不是2的n次幂，那么也一定不是4的n次幂。
+         * 对数n开平方，如果结果是偶数则是4的n次幂，不是则不是数的n次幂。
+         */
+
+        if (n == 0) return false;
+        if (n == 1) return true;
+        if (n > 0 && (n & (n - 1)) != 0) return false;
+        double v = Math.sqrt(n);
+        return v % 2 == 0;
+
+        /**
+         * 更加高级的一点想法：
+         * 如果n是4的幂，我们可以发现它除以3的余数一定为1。
+         */
+        // return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1;
     }
 }
 
