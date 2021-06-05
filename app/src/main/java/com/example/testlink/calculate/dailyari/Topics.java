086eb4f7d2111bbcf3cbfea2651d9235c8ed5992
foty <534728972@qq.com>
2021-06-05 10:39:16 +0800

    6-5 每日

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -3495,5 +3495,47 @@ public class Topics {
         }
         return headA;
     }
+
+    /**
+     * 203、移除链表元素
+     */
+    public ListNode removeElements(ListNode head, int val) {
+
+        /** 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+         *
+         *  示例 1：
+         * 输入：head = [1,2,6,3,4,5,6], val = 6
+         * 输出：[1,2,3,4,5]
+         *
+         * 示例 2：
+         * 输入：head = [], val = 1
+         * 输出：[]
+         *
+         * 示例 3：
+         * 输入：head = 0[7,7,7,7], val = 7
+         * 输出：[]
+         *  
+         * 提示：
+         * 列表中的节点在范围 [0, 104] 内
+         * 1 <= Node.val <= 50
+         * 0 <= k <= 50
+         */
+
+        /**
+         * 比较简单的一题了，唯一的点就是构建一个空节点，这样就不用考虑头结点被删除的情况了
+         */
+        if (head == null) return null;
+        ListNode nullNode = new ListNode(0);
+        nullNode.next = head;
+        ListNode pre = nullNode;
+        while (head != null) {
+            if (head.val == val) {
+                pre.next = head.next;
+            } else
+                pre = head;
+            head = head.next;
+        }
+        return nullNode.next;
+    }
 }
 
