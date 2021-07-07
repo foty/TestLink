5adc3afe238b1eb49396fcf65d65240a91a15b9f
foty <534728972@qq.com>
2021-07-07 17:57:49 +0800

    7-7 每日

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -4894,6 +4894,14 @@ public class Topics {
          * formula只包含字母、数字和圆括号，并且题目中给定的是合法的化学式。
          */
 
+        /**
+         * 思路：
+         * 利用栈保存()内的元素，从而整体地统计原子数量，最后使用TreeMap实现map的排序
+         * 发现几个好用的api ：
+         * Character.isDigit(char c) :判断该char是否为数字
+         * Character.isLowerCase(char c) :判断该char是否为小写字母
+         */
+
         //  "K4(ON(SO3)2)2"
         Stack<HashMap<String, Integer>> stack = new Stack<>();
         //先放入最外一层。
@@ -5050,6 +5058,48 @@ public class Topics {
             table.add(row);
         }
         return table;
+    }
+
+    /**
+     * 1711. 大餐计数
+     */
+    public int countPairs(int[] deliciousness) {
+
+        /**
+         *  大餐 是指 恰好包含两道不同餐品 的一餐，其美味程度之和等于 2 的幂。
+         * 你可以搭配 任意 两道餐品做一顿大餐。
+         * 给你一个整数数组 deliciousness ，其中 deliciousness[i] 是第 i​​​​​​​​​​​​​​ 道餐品的美味程度，返回你可以用数组中的餐品做出的不同 大餐 的数量。
+         * 结果需要对 10^9 + 7 取余。注意，只要餐品下标不同，就可以认为是不同的餐品，即便它们的美味程度相同。
+         *
+         * 示例 1：
+         * 输入：deliciousness = [1,3,5,7,9]
+         * 输出：4
+         * 解释：大餐的美味程度组合为 (1,3) 、(1,7) 、(3,5) 和 (7,9) 。
+         * 它们各自的美味程度之和分别为 4 、8 、8 和 16 ，都是 2 的幂。
+         * 示例 2：
+         * 输入：deliciousness = [1,1,1,3,3,3,7]
+         * 输出：15
+         * 解释：大餐的美味程度组合为 3 种 (1,1) ，9 种 (1,3) ，和 3 种 (1,7) 。
+         *
+         * 提示：
+         * 1 <= deliciousness.length <= 10^5
+         * 0 <= deliciousness[i] <= 2^20
+         */
+
+        /**
+         * 暴力双重循环直接超时。太秀了
+         */
+        int result = 0;
+        for (int i = 0; i < deliciousness.length; i++) {
+            for (int j = i + 1; j < deliciousness.length; j++) {
+                int count = deliciousness[i] + deliciousness[j];
+                if (count != 0 && (count & (count - 1)) == 0) {
+                    result++;
+                    result %= 1000000007; // 防止越界?
+                }
+            }
+        }
+        return result;
     }
 }
 
