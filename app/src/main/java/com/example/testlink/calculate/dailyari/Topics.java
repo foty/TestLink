d492576fbd6bb1d7dddcf5c70d926f5110fa55f8
foty <534728972@qq.com>
2021-07-20 15:40:50 +0800

    7-18 每日

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -5436,5 +5436,45 @@ public class Topics {
         return max;
     }
 
+    /**
+     * 面试题 10.02、变位词组
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        /**
+         * 编写一种方法，对字符串数组进行排序，将所有变位词组合在一起。变位词是指字母相同，但排列不同的字符串。
+         * 注意：本题相对原题稍作修改
+         *
+         * 示例:
+         * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+         * 输出:
+         * [
+         *   ["ate","eat","tea"],
+         *   ["nat","tan"],
+         *   ["bat"]
+         * ]
+         *
+         * 说明：
+         * 所有输入均为小写字母。
+         * 不考虑答案输出的顺序。
+         */
+
+        /**
+         * 思路：
+         * 题目意思是将所有字符相同的组成的不同字符串归为一类。官方思路是：既然所有字符都一样，那么不同字符串排序后的字符串肯定
+         * 是一样的，可以以此作为key，存放相同字符组成的字符串。关键点排序。
+         */
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
 }
 
