09b5b1e5e88ef71e5e57926ca9c4e4c2069670af
foty <534728972@qq.com>
2021-08-02 16:18:04 +0800

    8-1 每日

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -5644,5 +5644,94 @@ public class Topics {
         }
         return sub;
     }
+
+    /**
+     * 987、 二叉树的垂序遍历
+     */
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+        /**
+         *  给你二叉树的根结点 root ，请你设计算法计算二叉树的 垂序遍历 序列。
+         *
+         * 对位于 (row, col) 的每个结点而言，其左右子结点分别位于 (row + 1, col - 1) 和 (row + 1, col + 1) 。树的根结点位于 (0, 0) 。
+         * 二叉树的 垂序遍历 从最左边的列开始直到最右边的列结束，按列索引每一列上的所有结点，形成一个按出现位置从上到下排序的有序列表。如果同行同列
+         * 上有多个结点，则按结点的值从小到大进行排序。
+         * 返回二叉树的 垂序遍历 序列。
+         *
+         * 示例 1：
+         * 输入：root = [3,9,20,null,null,15,7]
+         * 输出：[[9],[3,15],[20],[7]]
+         * 解释：
+         * 列 -1 ：只有结点 9 在此列中。
+         * 列  0 ：只有结点 3 和 15 在此列中，按从上到下顺序。
+         * 列  1 ：只有结点 20 在此列中。
+         * 列  2 ：只有结点 7 在此列中。
+         *
+         * 示例 2：
+         * 输入：root = [1,2,3,4,5,6,7]
+         * 输出：[[4],[2],[1,5,6],[3],[7]]
+         * 解释：
+         * 列 -2 ：只有结点 4 在此列中。
+         * 列 -1 ：只有结点 2 在此列中。
+         * 列  0 ：结点 1 、5 和 6 都在此列中。
+         *           1 在上面，所以它出现在前面。
+         *           5 和 6 位置都是 (2, 0) ，所以按值从小到大排序，5 在 6 的前面。
+         * 列  1 ：只有结点 3 在此列中。
+         * 列  2 ：只有结点 7 在此列中。
+         *
+         * 示例 3：
+         * 输入：root = [1,2,3,4,6,5,7]
+         * 输出：[[4],[2],[1,5,6],[3],[7]]
+         * 解释：
+         * 这个示例实际上与示例 2 完全相同，只是结点 5 和 6 在树中的位置发生了交换。
+         * 因为 5 和 6 的位置仍然相同，所以答案保持不变，仍然按值从小到大排序。
+         *
+         * 提示：
+         * 树中结点数目总数在范围 [1, 1000] 内
+         * 0 <= Node.val <= 1000
+         */
+
+        /**
+         * 思路：哈希表+排序。排序规则：先排列号，从小到大，相同则按行号从小到大；再排行号，相同则按value从小到大
+         */
+
+        map.put(root, new int[]{0, 0, root.val});
+        dfs987(root);
+
+        List<int[]> list = new ArrayList<>(map.values());
+        Collections.sort(list, (a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0]; // 列号排序
+            if (a[1] != b[1]) return a[1] - b[1]; // 行号排序
+            return a[2] - b[2]; // 内容排序
+        });
+
+        int n = list.size();
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; ) {
+            int j = i;
+            List<Integer> tmp = new ArrayList<>();
+            while (j < n && list.get(j)[0] == list.get(i)[0]) tmp.add(list.get(j++)[2]);
+            ans.add(tmp);
+            i = j;
+        }
+        return ans;
+    }
+
+    Map<TreeNode, int[]> map = new HashMap<>(); // col, row, val
+
+    private void dfs987(TreeNode root) {
+        if (root == null) return;
+        int[] info = map.get(root);
+        int col = info[0], row = info[1], val = info[2];
+        if (root.left != null) {
+            map.put(root.left, new int[]{col - 1, row + 1, root.left.val});
+            dfs987(root.left);
+        }
+        if (root.right != null) {
+            map.put(root.right, new int[]{col + 1, row + 1, root.right.val});
+            dfs987(root.right);
+        }
+    }
+
 }
 
