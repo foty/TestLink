d38b7ae02b710a3c73227bf95869b54816334a48
foty <534728972@qq.com>
2021-08-04 16:18:33 +0800

    8-3 每日

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -5871,5 +5871,62 @@ public class Topics {
         return ans == INF ? -1 : ans;
     }
 
+    /**
+     * 581、 最短无序连续子数组
+     */
+    public int findUnsortedSubarray(int[] nums) {
+        /**
+         *  给你一个整数数组 nums ，你需要找出一个 连续子数组 ，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+         * 请你找出符合题意的 最短 子数组，并输出它的长度。
+         *
+         * 示例 1：
+         * 输入：nums = [2,6,4,8,10,9,15]
+         * 输出：5
+         * 解释：你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
+         *
+         * 示例 2：
+         * 输入：nums = [1,2,3,4]
+         * 输出：0
+         *
+         * 示例 3：
+         * 输入：nums = [1]
+         * 输出：0
+         *
+         * 提示：
+         * 1 <= nums.length <= 104
+         * -105 <= nums[i] <= 105
+         */
+        if (nums.length <= 1) return 0;
+
+        /**
+         * 思路：
+         * 整个表都要是升序的，那么从左到右，或者从右到左，必然右边的数比左边的大；如果出现左边比右边大的数那么这个数就是要调整的位置。
+         * 这样一来左右2边各确定一个位置，中间的数都是需要调整的，统计个数即可。注意，从左到右比较最大值，表示此值右边都是升序的；从右到左比
+         * 较最小值，表示此值左边都是升序的。这点理解很重要。
+         */
+
+        int left = 0;
+        int right = -1; // 为什么要等于-1，因为如果是一个升序数组的话，left，right都不会被赋值，(right - left + 1)结果却等于了1。
+        int max = nums[0];
+        int min = nums[nums.length - 1];
+        for (int i = 0; i < nums.length; i++) {
+            //从左到右
+            if (nums[i] < max) { //正常顺序是要比max大
+                right = i;
+            } else {
+                max = nums[i];
+            }
+
+            // 从右到左
+            if (nums[nums.length - 1 - i] > min) { //正常顺序是要比min小
+                left = nums.length - 1 - i;
+            } else {
+                min = nums[nums.length - 1 - i];
+            }
+
+        }
+
+        return right - left + 1;
+    }
 }
 
