0f3fc44aa0cb9f458b6de1c3680a27edc7aafb46
foty <534728972@qq.com>
2021-09-05 21:59:09 +0800

    9-05 每日

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -1,8 +1,6 @@
 package com.example.testlink.calculate.dailyari;
 
 import com.example.testlink.calculate.sword_for_offer.ListNode;
-import com.example.testlink.calculate.sword_for_offer.T1;
-import com.example.testlink.calculate.sword_for_offer.T2;
 import com.example.testlink.calculate.sword_for_offer.TreeNode;
 
 import java.util.ArrayList;
@@ -7140,6 +7138,41 @@ public class Topics {
 //            result[i] = arr[i];
 //        }
         return result;
+    }
+
+    /**
+     * 470、用 Rand7() 实现 Rand10()
+     */
+    public int rand10() {
+
+        /**
+         * 已有方法 rand7 可生成 1 到 7 范围内的均匀随机整数，试写一个方法 rand10 生成 1 到 10 范围内的均匀随机整数。
+         * 不要使用系统的 Math.random() 方法。
+         *
+         * 示例 1:
+         * 输入: 1
+         * 输出: [7]
+         *
+         * 示例 2:
+         * 输入: 2
+         * 输出: [8,4]
+         *
+         * 示例 3:
+         * 输入: 3
+         * 输出: [8,1,10]
+         *
+         * 提示:
+         * rand7 已定义。
+         * 传入参数: n 表示 rand10 的调用次数。
+         * 进阶:
+         * rand7()调用次数的 期望值 是多少 ?
+         * 你能否尽量少调用 rand7() ?
+         */
+//        while (true) {
+//            int ans = (rand7() - 1) * 7 + (rand7() - 1); // 进制转换
+//            if (1 <= ans && ans <= 10) return ans;
+//        }
+        return 0;
     }
 
 
