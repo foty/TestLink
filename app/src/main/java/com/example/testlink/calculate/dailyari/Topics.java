c13b1fdb3ee7fbae2845fdd69235261b6b597a24
foty <534728972@qq.com>
2021-10-10 21:06:57 +0800

    10-10

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -8115,5 +8115,25 @@ public class Topics {
         SummaryRanges s = new SummaryRanges();
     }
 
+    /**
+     * 441、排列硬币
+     */
+    public int arrangeCoins(int n) {
+        /**
+         * 你总共有 n 枚硬币，并计划将它们按阶梯状排列。对于一个由 k 行组成的阶梯，其第 i 行必须正好有 i 枚硬币。阶梯的最后一行 可能 是不完整的。
+         * 给你一个数字 n ，计算并返回可形成 完整阶梯行 的总行数。
+         */
+        /**
+         * 简单题，模拟即可
+         */
+        int i = 1;
+        while (true) {
+            n = n - i;
+            i++;
+            if (n == i) return i;
+            if (n < i) return i - 1;
+        }
+    }
+
 }
 
