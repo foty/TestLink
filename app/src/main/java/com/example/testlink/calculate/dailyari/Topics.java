2e96d185ab712cebe0f019768c6defed4723b33e
foty <534728972@qq.com>
2021-11-05 16:03:58 +0800

    提交题目

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -8751,6 +8751,49 @@ public class Topics {
         return 0;
     }
 
+    /**
+     * 1218、最长定差子序列
+     */
+    public int longestSubsequence(int[] arr, int difference) {
+
+        /**
+         * 给你一个整数数组 arr 和一个整数 difference，请你找出并返回 arr 中最长等差子序列的长度，该子序列中相邻元素之间的差等于 difference 。
+         * 子序列 是指在不改变其余元素顺序的情况下，通过删除一些元素或不删除任何元素而从 arr 派生出来的序列。
+         *
+         * 示例 1：
+         * 输入：arr = [1,2,3,4], difference = 1
+         * 输出：4
+         * 解释：最长的等差子序列是 [1,2,3,4]。
+         *
+         * 示例 2：
+         * 输入：arr = [1,3,5,7], difference = 1
+         * 输出：1
+         * 解释：最长的等差子序列是任意单个元素。
+         *
+         * 示例 3：
+         * 输入：arr = [1,5,7,8,5,3,4,2,1], difference = -2
+         * 输出：4
+         * 解释：最长的等差子序列是 [7,5,3,1]。
+         *
+         * 提示：
+         * 1 <= arr.length <= 105
+         * -104 <= arr[i], difference <= 104
+         */
+
+        /**
+         * 双层循环超时，使用动态规划。
+         */
+        int ans = 1;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i - difference, 0) + 1);
+            ans = Math.max(ans, map.get(i));
+        }
+        return ans;
+
+
+    }
+
 
 }
 
