65c06150d7de12fbd49fffcaae9d5c5a972b6023
foty <534728972@qq.com>
2021-11-23 10:29:33 +0800

    提交题目

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -9228,6 +9228,53 @@ public class Topics {
         Subject384 su = new Subject384(new int[]{3, 5, 8, 4, 2, 1, 7});
     }
 
+    /**
+     * 859、亲密字符串
+     */
+    public boolean buddyStrings(String s, String goal) {
+        /**
+         * 给你两个字符串 s 和 goal ，只要我们可以通过交换 s 中的两个字母得到与 goal 相等的结果，就返回 true ；否则返回 false 。
+         * 交换字母的定义是：取两个下标 i 和 j （下标从 0 开始）且满足 i != j ，接着交换 s[i] 和 s[j] 处的字符。
+         * 例如，在 "abcd" 中交换下标 0 和下标 2 的元素可以生成 "cbad" 。
+         *
+         * 示例 1：
+         * 输入：s = "ab", goal = "ba"
+         * 输出：true
+         * 解释：你可以交换 s[0] = 'a' 和 s[1] = 'b' 生成 "ba"，此时 s 和 goal 相等。
+         *
+         * 示例 2：
+         * 输入：s = "ab", goal = "ab"
+         * 输出：false
+         * 解释：你只能交换 s[0] = 'a' 和 s[1] = 'b' 生成 "ba"，此时 s 和 goal 不相等。
+         *
+         * 示例 3：
+         * 输入：s = "aa", goal = "aa"
+         * 输出：true
+         * 解释：你可以交换 s[0] = 'a' 和 s[1] = 'a' 生成 "aa"，此时 s 和 goal 相等。
+         *
+         * 示例 4：
+         * 输入：s = "aaaaaaabc", goal = "aaaaaaacb"
+         * 输出：true
+         *
+         * 提示：
+         * 1 <= s.length, goal.length <= 2 * 104
+         * s 和 goal 由小写英文字母组成
+         */
+
+        /**
+         * 思路：分类讨论(长度不等必然false；字符不相等也是false；当且只有2个不相等的字符为true
+         * 或者没有不相等的字符但是有2个以上相等重复字符为true)
+         */
+
+        if(s.length() != goal.length()) return false;
+        // 判断所有组成字符是否相等。
+
+        // 判断是否只有2个字符位置不一样
+
+        // 或者统计相同字符串是否大于等于2个
+
+        return false;
+    }
 
 }
 
