04e7572632a722b7c12dff7c1a5dedd6c5c3414b
foty <534728972@qq.com>
2021-12-07 15:42:58 +0800

    12-7

--- app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
+++ app/src/main/java/com/example/testlink/calculate/dailyari/Topics.java
@@ -9644,5 +9644,85 @@ public class Topics {
         return sum;
     }
 
+    /**
+     * 1034、边界着色
+     */
+    public static int[][] colorBorder(int[][] grid, int row, int col, int color) {
+        /**
+         * 给你一个大小为 m x n 的整数矩阵 grid ，表示一个网格。另给你三个整数 row、col 和 color 。网格中的每个值表示该位置处的网格块的颜色。
+         * 当两个网格块的颜色相同，而且在四个方向中任意一个方向上相邻时，它们属于同一 连通分量 。
+         * 连通分量的边界 是指连通分量中的所有与不在分量中的网格块相邻（四个方向上）的所有网格块，或者在网格的边界上（第一行/列或最后一行/列）的所有网格块。
+         * 请你使用指定颜色 color 为所有包含网格块 grid[row][col] 的 连通分量的边界 进行着色，并返回最终的网格 grid 。
+         *
+         * 示例 1：
+         * 输入：grid = [[1,1],[1,2]], row = 0, col = 0, color = 3
+         * 输出：[[3,3],[3,2]]
+         *
+         * 示例 2：
+         * 输入：grid = [[1,2,2],[2,3,2]], row = 0, col = 1, color = 3
+         * 输出：[[1,3,3],[2,3,3]]
+         *
+         * 示例 3：
+         * 输入：grid = [[1,1,1],[1,1,1],[1,1,1]], row = 1, col = 1, color = 2
+         * 输出：[[2,2,2],[2,1,2],[2,2,2]]
+         *
+         * 提示：
+         * m == grid.length
+         * n == grid[i].length
+         * 1 <= m, n <= 50
+         * 1 <= grid[i][j], color <= 1000
+         * 0 <= row < m
+         * 0 <= col < n
+         */
+
+        // 确定所有的连通分量。
+        // 对连通分量集合的边界染色。
+
+        int[][] visit = new int[grid.length][grid[0].length];
+
+        dfs1034(grid, row, col, visit, grid[row][col]);
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (visit[i][j] == 1) {
+                    grid[i][j] = color;
+                }
+            }
+        }
+        return grid;
+    }
+
+    private static boolean dfs1034(int[][] g, int row, int col, int[][] visit, int sameC) {
+        // 处理边界问题
+        if (row < 0) return false;
+        if (row >= g.length) return false;
+        if (col < 0) return false;
+        if (col >= g[0].length) return false;
+
+        if (visit[row][col] == 0) { // 找没有访问过的
+            if (g[row][col] == sameC)
+                visit[row][col] = 1;
+            else {
+                visit[row][col] = 2;
+                return false;
+            }
+        } else
+            return g[row][col] == sameC;
+
+        //上
+        boolean t = dfs1034(g, row - 1, col, visit, sameC);
+        //下
+        boolean b = dfs1034(g, row + 1, col, visit, sameC);
+        //左
+        boolean l = dfs1034(g, row, col - 1, visit, sameC);
+        //右
+        boolean r = dfs1034(g, row, col + 1, visit, sameC);
+
+        if (t && b && l && r)  // 不是边界，不染色
+            visit[row][col] = 2;
+
+        return g[row][col] == sameC;
+    }
+
 }
 
